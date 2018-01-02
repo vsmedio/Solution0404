7ee1e26dedeef3f4572125dd1362014f422ce1b3
Arvis Abromovics <info@vsmedio.lv>
2018-01-02 20:36:10 +0100

    Pievienoju latvisko tulkojumu uzdevuma aprakstam.

--- 1.JavaSyntax/src/com/javarush/task/task04/task0403/Cat.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0403/Cat.java
@@ -10,6 +10,17 @@ package com.javarush.task.task04.task0403;
 2. Переменная name должна иметь модификатор доступа private и тип String.
 3. Класс Cat должен содержать только два метода setName и main.
 4. Метод setName класса Cat должен устанавливать значение переменной private String name равным переданному параметру String name.
+
+//Latviski:
+
+Ieviest setName metodi, lai tā varētu iestatīt privātā String nosaukuma mainīgā vērtību uz nodoto parametru String name.
+
+
+Prasības:
+1. Cat klasei ir jābūt tikai vienam mainīgajam nosaukumam.
+2. Mainīgo nosaukumam ir jābūt privātam piekļuves modificētājam un String tipam.
+3. Cat klases sastāvā ir tikai divas metodes, setName un galvenā.
+4. Cat klases class setName metodei jānosaka privātā String nosaukuma mainīgā vērtība nodotajam parametram String name.
 */
 
 public class Cat {
